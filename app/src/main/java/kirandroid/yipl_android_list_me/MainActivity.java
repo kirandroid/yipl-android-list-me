d386b5286b26782acd27a44267251fb883f96724
Kirandroid <pradhanangakiran@gmail.com>
2017-12-01 19:39:40 -0800

    Initial commit

---
+++ app/src/main/java/kirandroid/yipl_android_list_me/MainActivity.java
@@ -0,0 +1,13 @@
+package kirandroid.yipl_android_list_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
